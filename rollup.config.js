46076a2337411ee34309ad3c2b2c94461764a2fd
dazzlec123 <dazzlec123@gmail.com>
2024-09-18 19:17:43 +0000

    Sep 18, 2024, 3:17 PM

--- rollup.config.js
+++ rollup.config.js
@@ -18,7 +18,7 @@ export default [
         extensions: ['.ts'],
         plugins: ['@babel/plugin-syntax-import-assertions'],
         babelHelpers: 'bundled',
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env', '@babel/preset-flow']
       })
     ]
   },
