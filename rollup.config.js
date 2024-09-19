b3362a40b90f7b1e344b59050018c98348368b9a
dazzlec123 <dazzlec123@gmail.com>
2024-09-19 01:28:25 +0000

    Sep 18, 2024, 9:28 PM

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import { babel } from '@rollup/plugin-babel'
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default [
   {
